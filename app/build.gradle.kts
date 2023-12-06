7349e38a0ece78406c0e8b14dcdefa96bb219514
LightTMer <saniaersh@gmail.com>
2023-12-06 22:54:02 +0500

    Menu and buttons_

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,25 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
 }
 
 android {
     namespace = "com.example.cinemaview"
-    compileSdk = 33
+    compileSdk = 34
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
 
     defaultConfig {
         applicationId = "com.example.cinemaview"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -25,6 +34,7 @@ android {
                 "proguard-rules.pro"
             )
         }
+
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -36,11 +46,20 @@ android {
 }
 
 dependencies {
+    val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    implementation("androidx.compose.material3:material3")
+//    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.compose.ui:ui-android:1.5.1")
+
+//    implementation("androidx.compose.compiler:compiler:1.9.20")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/cinemaview/AboutApp.kt
+++ app/src/main/java/com/example/cinemaview/AboutApp.kt
@@ -1,11 +1,139 @@
 package com.example.cinemaview
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class AboutApp : AppCompatActivity() {
+    lateinit var composeView: ComposeView
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_app)
+
+
+        composeView = findViewById(R.id.compose_view)
+        composeView.setContent {
+            Box(
+                modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+            ) {
+                InformationScreen()
+            }
+        }
+        val constraintLayout = findViewById(R.id.layout) as LinearLayout
+        val animationDrawable = constraintLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2000)
+        animationDrawable.setExitFadeDuration(4000)
+        animationDrawable.start()
+    }
+
+    @Composable
+    fun InformationScreen() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Наша команда")
+
+            Row(
+                modifier = Modifier.padding(16.dp)
+            )
+            {
+                    Image(
+                        painter = painterResource(id = R.drawable.alexx),
+                        contentDescription = "Image 1",
+                        modifier = Modifier
+                            .size(140.dp)
+                            .clip(CircleShape)
+//                            .offset(x=-5.dp)
+//                        v.width(200.dp).height(200.dp)
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.ilya),
+                        contentDescription = "Image 2",
+                        modifier = Modifier
+                            .size(140.dp)
+                            .clip(CircleShape)
+//                            .offset(x=5.dp)
+
+                    )
+            }
+            Row(
+                modifier = Modifier.padding(16.dp),
+            )
+            {
+                Text(
+                    modifier = Modifier
+
+                        .padding(horizontal = 5.dp)
+                        .wrapContentWidth(Alignment.Start)
+                        .offset(x=-21.dp),
+                    text = "Ершов Александр"
+                )
+                Text(
+                    modifier = Modifier
+
+                        .padding(horizontal = 5.dp)
+                        .wrapContentWidth(Alignment.End)
+                        .offset(x=7.dp),
+                    text = "Потехин Илья"
+                )
+            }
+
+            Text(modifier = Modifier.padding(vertical = 30.dp), text ="CinemaView - это приложение, которое представляет собой сборник информации о разных фильмах, c их описанием и мировым рейтингом. По сути, это своего рода энциклопедия в мире кино!")
+            ElevatedButton(
+
+                onClick = { },
+                modifier = Modifier
+
+                    .height(50.dp)
+                    .width(200.dp)
+
+            ) {
+                Text("Перейти на Github")
+            }
+            ElevatedButton(
+                onClick = { onBackPressed() },
+                modifier = Modifier
+
+                    .height(50.dp)
+                    .offset(x = 0.dp, y = 120.dp)
+                    .width(200.dp)
+
+            ) {
+                Text("Назад")
+            }
+        }
     }
 }
